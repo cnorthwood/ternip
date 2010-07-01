34870a6c8e99c2afa0d8a09f496250a0b46821b6
Chris Northwood <chris@pling.org.uk>
2010-07-01 16:34:17 +0000

    Hopefully make unit tests more platform agnostic
    git-svn-id: http://svn.pling.org.uk/ternip/trunk@1725 046eaa17-6a8d-4e70-a5e5-287f5f62e04c

--- tests/formats/xml_doc.py
+++ tests/formats/xml_doc.py
@@ -12,4 +12,4 @@ class xml_doc_Test(unittest.TestCase):
     def test_strip_timexes(self):
         t = _xml_doc("""<?xml version="1.0" ?><root>This is some <FOO attr="timex">annotated <FOO>embedded annotated </FOO>text</FOO>.</root>""")
         t.strip_timexes()
-        self.assertEquals(str(t),"""<?xml version="1.0" ?><root>This is some annotated embedded annotated text.</root>""")
+        self.assertTrue(str(t).find("<root>This is some annotated embedded annotated text.</root>") > -1)
