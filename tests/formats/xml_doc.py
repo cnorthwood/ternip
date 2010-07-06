e487f8a5366cb489a00cd706b984b135d2e4caf5
Chris Northwood <chris@pling.org.uk>
2010-07-06 01:06:19 +0000

    More resilient unit test
    git-svn-id: http://svn.pling.org.uk/ternip/trunk@1728 046eaa17-6a8d-4e70-a5e5-287f5f62e04c

--- tests/formats/xml_doc.py
+++ tests/formats/xml_doc.py
@@ -2,6 +2,7 @@
 
 import unittest
 import ternip.formats.xml_doc
+import xml.dom.minidom
 
 class _xml_doc(ternip.formats.xml_doc.xml_doc):
     
@@ -12,4 +13,4 @@ class xml_doc_Test(unittest.TestCase):
     def test_strip_timexes(self):
         t = _xml_doc("""<?xml version="1.0" ?><root>This is some <FOO attr="timex">annotated <FOO>embedded annotated </FOO>text</FOO>.</root>""")
         t.strip_timexes()
-        self.assertTrue(str(t).find("<root>This is some annotated embedded annotated text.</root>") > -1)
+        self.assertEquals(str(t), xml.dom.minidom.parseString("<root>This is some annotated embedded annotated text.</root>").toxml())
