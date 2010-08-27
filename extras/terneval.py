dc126b8694f35b4c12615d31f5ccb187b7cae253
Chris Northwood <chris@pling.org.uk>
2010-08-27 20:17:02 +0000

    Microaveraging of GUTime Update evaluation section following feedback
    git-svn-id: http://svn.pling.org.uk/ternip/trunk@1941 046eaa17-6a8d-4e70-a5e5-287f5f62e04c

--- extras/terneval.py
+++ extras/terneval.py
@@ -27,7 +27,31 @@ def get_f_measure(text, type):
         if type != None and overall_found and line[7:].startswith(type):
             return float(line[-5:])
         i += 1
-    return float(1)
+    return float(0)
+
+def get_bits(text, type):
+    overall_found = False
+    i = 0
+    lines = text.splitlines()
+    for line in lines:
+        if not overall_found and line[:9] == '  overall':
+            overall_found = True
+            if type == None:
+                l = lines[i+2].split()
+                if len(l) > 4:
+                    return (int(l[1]), int(l[2]), int(l[4]))
+                else:
+                    return (0,0,0)
+        if type != None and overall_found and line[7:].startswith(type):
+            l = line.split()
+            return (int(l[1]), int(l[2]), int(l[4]))
+        i += 1
+    return (0,0,0)
+
+def compute_f_measure(pos, act, cor):
+    prec = float(cor)/float(act)
+    rec = float(cor)/float(pos)
+    return (2 * prec * rec) / (prec + rec)
 
 print
 print "TERNIP TERN evaluator"
@@ -51,6 +75,15 @@ print
 ternip_recognition_scores = []
 ternip_extent_scores = []
 ternip_norm_scores = []
+ternip_recognition_poss = 0
+ternip_recognition_acts = 0
+ternip_recognition_cors = 0
+ternip_extent_poss = 0
+ternip_extent_acts = 0
+ternip_extent_cors = 0
+ternip_norm_poss = 0
+ternip_norm_acts = 0
+ternip_norm_cors = 0
 
 start = time.clock()
 
@@ -72,7 +105,7 @@ for i in range(len(unannotated)):
     dct_sents = recogniser.tag(dct_sents)
     normaliser.annotate(dct_sents, 'XXXXXXXX')
     doc.reconcile_dct(dct_sents)
-    if len(dct_sents) > 0 and len(dct_sents[0][0][2]) > 0:
+    if len(dct_sents) > 0 and len(dct_sents[0]) > 0 and len(dct_sents[0][0][2]) > 0:
         dct = dct_sents[0][0][2].pop().value
     else:
         dct = ''
@@ -100,11 +133,21 @@ for i in range(len(unannotated)):
     ternip_recognition_scores.append(ternip_recognition_score)
     ternip_extent_scores.append(ternip_extent_score)
     ternip_norm_scores.append(ternip_norm_score)
+    (pos, act, cor) = get_bits(output, None)
+    ternip_recognition_poss += pos
+    ternip_recognition_acts += act
+    ternip_recognition_cors += cor
+    (pos, act, cor) = get_bits(output, 'TEXT')
+    ternip_extent_poss += pos
+    ternip_extent_acts += act
+    ternip_extent_cors += cor
+    (pos, act, cor) = get_bits(output, 'VAL')
+    ternip_norm_poss += pos
+    ternip_norm_acts += act
+    ternip_norm_cors += cor
 
 print
 print "MACROAVERAGED F-MEASURES"
-
-# Macroaveraging
 print "    successfully tagged", len(ternip_recognition_scores), "documents"
 print "    recognition", sum(ternip_recognition_scores)/len(ternip_recognition_scores)
 print "    extent", sum(ternip_extent_scores)/len(ternip_extent_scores)
@@ -112,4 +155,12 @@ print "    normalisation", sum(ternip_norm_scores)/len(ternip_norm_scores)
 print "    run time (CPU seconds)", time.clock() - start
 print
 
+
+print
+print "MICROAVERAGED F-MEASURES"
+print "    successfully tagged", len(ternip_recognition_scores), "documents"
+print "    recognition", compute_f_measure(ternip_recognition_poss, ternip_recognition_acts, ternip_recognition_cors)
+print "    extent", compute_f_measure(ternip_extent_poss, ternip_extent_acts, ternip_extent_cors)
+print "    normalisation", compute_f_measure(ternip_norm_poss, ternip_norm_acts, ternip_norm_cors)
+print
 shutil.rmtree(temp)
